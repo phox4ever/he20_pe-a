14b6c217d9c234b2e8d76fcb943400127043d384
Philipp Martin <phox4ever@gmail.com>
2022-05-31 19:36:26 +0200

    blog initial

---
+++ blog/src/test/java/ch/hfict/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hfict.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
