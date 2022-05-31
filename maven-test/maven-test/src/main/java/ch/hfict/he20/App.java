3d8ea993852dad5612bafa9591f9f4bd5c0a7aba
Philipp Martin <phox4ever@gmail.com>
2022-05-31 19:37:18 +0200

    maven test

---
+++ maven-test/maven-test/src/main/java/ch/hfict/he20/App.java
@@ -0,0 +1,21 @@
+package ch.hfict.he20;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        Configurator.setRootLevel(Level.DEBUG);
+        logger.debug("Indeed!");
+    }
+}
---
+++ maven-test/maven-test/src/test/java/ch/hfict/he20/AppTest.java
@@ -0,0 +1,20 @@
+package ch.hfict.he20;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
